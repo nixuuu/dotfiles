8245f845969a1356355272c8d27c123df191f985
Damian Plewa <damianp96@gmail.com>
2022-02-25 18:06:14 +0100

    added nitrogen configa and wallpapers

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 ln -s $PWD/.Xresources ~/.Xresources
 mkdir -p ~/.config
 
-configs=(alacritty i3 polybar rofi)
+configs=(alacritty i3 polybar rofi nitrogen)
 
 for pkg in "${configs[@]}"
 do
@@ -14,6 +14,9 @@ ln -s $PWD/.config/picom.conf ~/.config/picom.conf
 
 ln -s $PWD/.mpd ~/.mpd
 touch ~/.mpd/mpd.db ~/.mpd/mpd.pid ~/.mpd/mpdstate
+
+mkdir -p ~/Pictures
+ln -s $PWD/wallpapers ~/Pictures/wallpapers
 
 mkdir -p ~/.local/share/fonts
 ln -s $PWD/fonts ~/.local/share/fonts/nixdots
